5b595d95ad04cbc1ecc5da875ea089c1ea2c7803
Bill Khonkhammy <bill.khk@gmail.com>
2025-05-22 10:38:30 +0200

    Dynamic form with Jinja

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
-app=Flask(__name__)
+dict_url = {
+    'France': "https://www.amazon.fr/",
+    'Germany': "https://www.amazon.de/",
+    'Belgium': "https://www.amazon.com.be/",
+}
+
+app = Flask(__name__)
+
+
 @app.route("/")
 def hello():
+    return render_template('home.html', dict_url=dict_url)
+
+@app.route("/search/<item>&<code>")
+def search(item, code):
+    print(f'item: {item}')
+    print(f'code: {code}')
     return render_template('home.html')
 
 
